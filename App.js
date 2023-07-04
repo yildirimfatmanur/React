c2e1b3c75cb25adfc03bb7267d294f62768747ca
Fatma Nur Yıldırım <126201930+yildirimfatmanur@users.noreply.github.com>
2023-07-04 15:29:15 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,60 @@
+//import logo from './logo.svg';
+import React,{useState} from 'react'
+import './App.css'
+import { BsBackspace } from "react-icons/bs";
+
+
+
+function App() {
+  const [value,setValue] = useState('');
+  return (
+    <div className='container'>
+      <div className='calculator'>
+        <form action=''>
+          <div className='display'>
+            <input type='text' value={value}/>
+          </div>
+          <div style={{display: 'flex', justifyContent: 'flex-end'}}>
+            <input  type='button' value='DEL' onClick={e=>setValue(value.slice(0,-1))}></input>
+          </div>
+          <div>
+            <input type='button' value='C' onClick={e=>setValue('')}/>
+            <input type='button' value='()'onClick={e=>setValue(value + e.target.value)}/>
+            <input type='button' value='%'onClick={e=>setValue(value + e.target.value)}/>
+            <input type='button' value='/'onClick={e=>setValue(value + e.target.value)}/>
+          </div>
+
+          <div>
+            <input type='button' value='7' onClick={e=>setValue(value + e.target.value)}/>
+            <input type='button' value='8' onClick={e=>setValue(value + e.target.value)}/>
+            <input type='button' value='9' onClick={e=>setValue(value + e.target.value)}/>
+            <input type='button' value='*' onClick={e=>setValue(value + e.target.value)}/>
+          </div>
+
+          <div>
+            <input type='button' value='4' onClick={e=>setValue(value + e.target.value)}/>
+            <input type='button' value='5' onClick={e=>setValue(value + e.target.value)}/>
+            <input type='button' value='6' onClick={e=>setValue(value + e.target.value)}/>
+            <input type='button' value='-' onClick={e=>setValue(value + e.target.value)}/>
+          </div>
+
+          <div>
+            <input type='button' value='1' onClick={e=>setValue(value + e.target.value)}/>
+            <input type='button' value='2' onClick={e=>setValue(value + e.target.value)}/>
+            <input type='button' value='3' onClick={e=>setValue(value + e.target.value)}/>
+            <input type='button' value='+' onClick={e=>setValue(value + e.target.value)}/>
+          </div>
+
+          <div>
+            <input type='button' value='+/-'onClick={e=>setValue(value + e.target.value)}/>
+            <input type='button' value='0' onClick={e=>setValue(value + e.target.value)}/>
+            <input type='button' value='.' onClick={e=>setValue(value + e.target.value)}/>
+            <input type='button' value='=' onClick={e=>setValue(eval(value))}/>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default App;
